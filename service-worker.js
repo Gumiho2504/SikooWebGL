9aa8b7a23296f4a8132d000e53d490928775aa03
Gumiho2504 <hemchanmetrey123@gmail.com>
2024-11-27 14:49:32 +0700

    pwa

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'unity-pwa-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './Build/Build.json',
+  './Build/data.unityweb',
+  './Build/wasm.unityweb'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache).catch(error => {
+        console.error('Failed to cache some files:', error);
+      });
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
