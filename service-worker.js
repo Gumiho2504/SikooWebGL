0f299f48d19442ee4b766eab1b9b1df3d4d7f86c
Gumiho2504 <hemchanmetrey123@gmail.com>
2025-01-10 16:46:55 +0700

    progress

--- service-worker.js
+++ service-worker.js
@@ -18,12 +18,26 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request).then(response => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+
+self.addEventListener('fetch', (event) => {
+  // Mock progress example
+  let progress = 0;
+  const interval = setInterval(() => {
+    progress += 10;
+    self.clients.matchAll().then((clients) => {
+      clients.forEach((client) => {
+        client.postMessage({ url: event.request.url, progress });
+      });
+    });
+    if (progress >= 100) clearInterval(interval);
+  }, 500);
 });
 
 
